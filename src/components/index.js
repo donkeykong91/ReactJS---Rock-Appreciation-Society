ff3becfb8f4e19a93668dc5c36de0f90a95fe09b
donkeykong91 <darylnator@gmail.com>
2018-08-31 00:57:44 -0700

    Added two new components Left and Right

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,39 @@
+import MainMenu from "./ui/MainMenu";
+
+export const Left = function ({children}) {
+
+  return (
+
+    <div className="page">
+
+      <MainMenu className="main-menu" />
+
+      {chlidren}
+
+    </div>
+
+  );
+
+}
+
+
+export const Right = function ({children}) {
+
+  return (
+
+    <div className="page">
+
+      {chlidren}
+
+      <MainMenu className="main-menu" />
+
+    </div>
+
+  );
+
+}
+
+
 export const Whoops404 = ({ location }) =>
     <div>
         <h1>Whoops, resource not found</h1>
